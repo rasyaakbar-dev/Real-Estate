ec2305efdb900f5f15aa6867c56a434afc8a14ce
Rasya A.N. <229900790+rasyaakbar-dev@users.noreply.github.com>
2026-01-09 09:22:24 +0700

    thing update

---
+++ estate/README.md
@@ -0,0 +1,14 @@
+# Real-Estate
+
+this a testing for next big project
+
+me and rasya learn about fullstack web use odoo, owl, docker, postgree
+
+this is all shit becouse we dont know how the hell is work with source code pesantren
+
+tis is so as we thing
+
+2.0
+
+the test if its go or not, lemetry
+
---
+++ estate/__manifest__.py
@@ -0,0 +1,31 @@
+{
+    'name': "Real Estate",
+
+    'summary': "Module for managing real estate properties",
+
+    'description': """
+    This module allows you to manage real estate properties with various attributes
+    """,
+
+    'author': "My Company",
+    'website': "https://www.yourcompany.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    'depends': ['base'],
+
+    'data': [
+        'security/ir.model.access.csv',
+        'views/estate_property_view.xml',
+    ],
+    # only loaded in demonstration mode
+    # 'demo': [
+    #     'demo/demo.xml',
+    # ],
+    'application': True,
+}
+
