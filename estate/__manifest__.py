073ceab4b83f405ebc44307ffb00c0c72ede4630
Rasya A.N. <229900790+rasyaakbar-dev@users.noreply.github.com>
2026-01-12 08:43:53 +0700

    daily(?) update

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -7,13 +7,13 @@
     This module allows you to manage real estate properties with various attributes
     """,
 
-    'author': "My Company",
-    'website': "https://www.yourcompany.com",
+    'author': "Rasya A.N",
+    'website': "https://github.com/rasyaakbar-dev/Real-Estate",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Uncategorized',
+    'category': 'Marketing',
     'version': '0.1',
 
     'depends': ['base'],
--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -17,7 +17,7 @@ class EstateProperty(models.Model):
 
     name = fields.Char(string="Title",
                         required=True)
-    description = fields.Text()
+    description = fields.Text(string="Description")
     postcode = fields.Char(string="Postcode")
     date_availability = fields.Date(
         string="Available From",
@@ -34,10 +34,10 @@ class EstateProperty(models.Model):
     bedrooms = fields.Integer(string="Bedrooms"
                               , default=2)
     living_area = fields.Integer(string="Living Area (sqm)")
-    facades = fields.Integer()
-    garage = fields.Boolean()
-    garden = fields.Boolean()
-    garden_area = fields.Integer()
+    facades = fields.Integer(string="Facades")
+    garage = fields.Boolean(string="Garage")
+    garden = fields.Boolean(string="Garden")
+    garden_area = fields.Integer(string="Garden Area (sqm)")
     garden_orientation = fields.Selection(
         selection=[('north', 'North'), 
         ('south', 'South'), 
